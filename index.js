1d260041be2e60f69f5f0042a2686f20730ace40
Asif-Ruhani <asifruhani68@gmail.com>
2024-08-23 02:02:26 +0600

    changes

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 
-let LifeLine=3;
+let LifeLine=5;
+const life=document.getElementById('LifeRemain');
+life.innerText=LifeLine;
+
 let Score=0;
 
  
+
+
+
+
+function ScoreSection(){
+     
+   const totalScore=ScoreIncrement();
+   
+   const scoreBoard=document.getElementById('TotalScore');
+   scoreBoard.value=totalScore;
+}
+
+
+
+
+
+
+
+function EnterScoreSection(){
+   const PlayGround=document.getElementById('PlayGround');
+   PlayGround.classList.add('DisplayHidden');
+
+
+   const ScoreBoard=document.getElementById('ScoreScreen');
+   ScoreBoard.classList.remove('DisplayHidden');
+
+   ScoreSection();
+}
+
+
+
+
 function ContinueGame(){
    const AlphabetString='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
    const Alphabets=AlphabetString.split('');
@@ -21,33 +56,82 @@ function ContinueGame(){
 }
 
 
+
+
+
+
+
+
+
+
 function RemovePressedAlphabetBgColor(ID){
    const bgcolor=document.getElementById(ID);
    bgcolor.classList.remove('SetAlphabetBgColor');  // je button already correctly pressed hoyeche setar bg color remove kore dibe
 }
+
+
+
+
+
+
+
+function ScoreIncrement(){
+   const score=document.getElementById('TotalScore');
+   score.innerText=Score;
+   return Score;
+}
+
+
+
+
+
+
+
+function LifeLineDecrement(){
+   life.innerText=LifeLine;
+
+   if(LifeLine==0){
+      EnterScoreSection();
+   }
+}
+
+
+
+
+
+
+
+
 
 document.addEventListener('keyup', function(event){
    // console.log(event.key);    // kon key ta press korlam seta jante parbo "parameter.key" dile
    const PressedKey=event.key.toLocaleUpperCase();
 
    const getRandomAlphabet=document.getElementById('RandomAlphabet').innerText;
-   console.log('Random alphabet: ',getRandomAlphabet);
-   console.log('Your have pressed: ',PressedKey);
+   // console.log('Random alphabet: ',getRandomAlphabet);
+   // console.log('Your have pressed: ',PressedKey);
 
    if(getRandomAlphabet==PressedKey){
       // console.log('Right');
       RemovePressedAlphabetBgColor(PressedKey.toLocaleLowerCase());
       Score+=1;
-      console.log('Score: ',Score);
+      ScoreIncrement();
       ContinueGame();
    }
    else{
-      // console.log('Wrong');
       LifeLine-=1;
-      console.log('Life line: ',LifeLine);
+      LifeLineDecrement();
    }
 
 });   
+
+
+
+
+
+
+
+
 
 
 
