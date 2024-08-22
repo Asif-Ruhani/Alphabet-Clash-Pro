52448cb59c6d15aa6560dbdd51f67b283ce4972f
Asif-Ruhani <asifruhani68@gmail.com>
2024-08-22 22:42:03 +0600

    changes

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 
+function ContinueGame(){
+   const AlphabetString='abcdefghijklmnopqrstuvwxyz';
+   const Alphabets=AlphabetString.split('');
 
-function PlayNow(){
-   console.log('button is working'); 
+   const Random=Math.round(Math.random()*25);   // 26 letter, 0-25
+   const Alphabet=Alphabets[Random];
+
+   const displayAlphabet=document.getElementById('RandomAlphabet');
+   displayAlphabet.innerText=Alphabet;
+   console.log(Alphabet);
+
+   const AlphabetBgColor=document.getElementById(Alphabet);   // if we write ('Alphabet') with single quation, then it will not work.
+   // console.log(AlphabetBgColor);
+   AlphabetBgColor.classList.add('SetAlphabetBgColor');
+   console.log(AlphabetBgColor);
+}
+
+function EnterPlaySection(){
+  
+    const Score=document.getElementById('SomeScreen');
+    Score.classList.add('DisplayHidden');
+
    
+    const PlayGround=document.getElementById('PlayGround');
+    PlayGround.classList.remove('DisplayHidden');
+
+    ContinueGame();
 
 }
+
