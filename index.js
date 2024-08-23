3af4e9599bb568572655bd40e04bb49b53da9f6b
Asif-Ruhani <asifruhani68@gmail.com>
2024-08-23 12:54:27 +0600

    changes

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
 
-let LifeLine=5;
-const life=document.getElementById('LifeRemain');
-life.innerText=LifeLine;
 
-let Score=0;
 
- 
+ let LifeLine;
+ let Score;
 
 
 
 
 
-
-
-function ScoreSection(){
-     
-   const totalScore=ScoreIncrement();
-   
-   const scoreBoard=document.getElementById('TotalScore');
-   scoreBoard.value=totalScore;
-}
-
-
-
-
-
-
-
-function EnterScoreSection(){
-   const PlayGround=document.getElementById('PlayGround');
-   PlayGround.classList.add('DisplayHidden');
-
-
-   const ScoreBoard=document.getElementById('ScoreScreen');
-   ScoreBoard.classList.remove('DisplayHidden');
-
-   ScoreSection();
-}
-
-
-
-
-function ContinueGame(){
-   const AlphabetString='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-   const Alphabets=AlphabetString.split('');
-
-   const Random=Math.round(Math.random()*25);   // 26 letter, 0-25
-   const Alphabet=Alphabets[Random];
-
-   const displayRandomAlphabet=document.getElementById('RandomAlphabet');
-   displayRandomAlphabet.innerText=Alphabet;
-   // console.log(Alphabet);
-    const alphabet=Alphabet.toLocaleLowerCase();
-    const AlphabetBgColor=document.getElementById(alphabet);
-   // console.log(AlphabetBgColor);
-   AlphabetBgColor.classList.add('SetAlphabetBgColor');
-   // console.log(AlphabetBgColor);
-}
 
 
 
@@ -78,10 +29,37 @@ function RemovePressedAlphabetBgColor(ID){
 
 
 
-function ScoreIncrement(){
-   const score=document.getElementById('TotalScore');
-   score.innerText=Score;
-   return Score;
+// function ScoreIncrement(){
+   
+//    return Score;
+// }
+
+
+
+
+// function ScoreSection(){
+     
+//    const totalScore=ScoreIncrement();
+   
+//    const scoreBoard=document.getElementById('TotalScore');
+//    scoreBoard.value=totalScore;
+// }
+
+
+
+
+
+
+
+function EnterScoreSection(){
+   const PlayGround=document.getElementById('PlayGround');
+   PlayGround.classList.add('DisplayHidden');
+
+
+   const ScoreBoard=document.getElementById('ScoreScreen');
+   ScoreBoard.classList.remove('DisplayHidden');
+
+   // ScoreSection();
 }
 
 
@@ -89,17 +67,61 @@ function ScoreIncrement(){
 
 
 
+// function LifeLineDecrement(){
+  
+// }
+  
 
-function LifeLineDecrement(){
+
+
+
+
+function ContinueGame(){
+   const AlphabetString='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   const Alphabets=AlphabetString.split('');
+
+   const Random=Math.round(Math.random()*25);   // 26 letter, 0-25
+   const Alphabet=Alphabets[Random];
+
+   const displayRandomAlphabet=document.getElementById('RandomAlphabet'); 
+   displayRandomAlphabet.innerText=Alphabet;    // show random alphabet on the display
+   // console.log(Alphabet);
+
+    const LowerCaseAlphabet=Alphabet.toLocaleLowerCase();   // making all alphabets to lowercase for getting id, because ids are lowercased.
+    const getEachIdOfAlphabets=document.getElementById(LowerCaseAlphabet);   // we got each alphabet id from random alphabet.
+   // console.log(AlphabetBgColor);
+   getEachIdOfAlphabets.classList.add('SetAlphabetBgColor');  // add background color into pointed id.
+   // console.log(AlphabetBgColor);
+}
+
+
+function SetLifeAndScore(){
+   LifeLine=5;
+   const life=document.getElementById('LifeRemain');
    life.innerText=LifeLine;
-
-   if(LifeLine==0){
-      EnterScoreSection();
-   }
+   Score=0;
+   const CurrentScore=document.getElementById('TotalScore');
+   CurrentScore.innerText=Score;
 }
 
 
+function EnterPlaySection(){
+  
+    const HomeScreen=document.getElementById('HomeScreen');
+    HomeScreen.classList.add('DisplayHidden');
 
+    const ScoreScreen=document.getElementById('ScoreScreen');
+    ScoreScreen.classList.add('DisplayHidden');
+
+   
+    const PlayGround=document.getElementById('PlayGround');
+    PlayGround.classList.remove('DisplayHidden');
+    
+
+    SetLifeAndScore();
+
+    ContinueGame();  
+}
 
 
 
@@ -116,56 +138,26 @@ document.addEventListener('keyup', function(event){
 
    if(getRandomAlphabet==PressedKey){
       // console.log('Right');
-      RemovePressedAlphabetBgColor(PressedKey.toLocaleLowerCase());
+      RemovePressedAlphabetBgColor(getRandomAlphabet.toLocaleLowerCase());
       Score+=1;
-      ScoreIncrement();
       ContinueGame();
    }
    else{
       LifeLine-=1;
-      LifeLineDecrement();
+      const LifeRemain=document.getElementById('LifeRemain');
+      LifeRemain.innerText=LifeLine;
+
+      if(LifeLine==0){
+         RemovePressedAlphabetBgColor(getRandomAlphabet.toLocaleLowerCase());  // lifeline 0 hole je alphabet ta color hoyechilo setar color remove kore dibe, remove na korle pore abar khelte asle oi color ta age thekei roye jabe.
+         EnterScoreSection();
+      }
    }
 
-});   
+}); 
 
 
 
 
 
 
-
-
-
-
-
-function EnterPlaySection(){
-  
-    const Score=document.getElementById('SomeScreen');
-    Score.classList.add('DisplayHidden');
-
-   
-    const PlayGround=document.getElementById('PlayGround');
-    PlayGround.classList.remove('DisplayHidden');
-
-    ContinueGame();
-   
-
-}
-
-
-
-
-
-
-function PlayAgain(){
-
-   const PlayGround=document.getElementById('ScoreScreen');
-   PlayGround.classList.add('DisplayHidden');
-
-
-   const ScoreBoard=document.getElementById('PlayGround');
-   ScoreBoard.classList.remove('DisplayHidden');
-
-   ContinueGame();
-}
 
